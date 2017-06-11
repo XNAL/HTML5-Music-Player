93d66fcc4494df50729428491e2a1baf614700dd
Don <772528797@qq.com>
2017-06-11 19:25:38 +0800

    完善了音乐列表的功能
    完善了音乐列表的功能，部分代码优化了一点，并添加了一些注释。

--- js/music.js
+++ js/music.js
@@ -3,28 +3,35 @@ require([
 ], function(player) {
 	'use strict';
 	(function() {
-		// var play = new player();
-		// play.clList();
-		var $audio = document.getElementById('music-audio');
-		var $play = $('#btnPlay');
-		var $curProgress = $('.current-progress');
-		var $mode = $('#btnMode');
-		var $next = $('.prevNext');
-		var $musicList = $('#musicList');
-		var play = new player.MusicPlayer($audio, $play, $curProgress, $musicList);
+		var $audio = document.getElementById('music-audio'),
+			$play = $('#btnPlay'),
+			$curProgress = $('.current-progress'),
+			$progress = $('.progress'),
+			$mode = $('#btnMode'),
+			$next = $('.prevNext'),
+			$musicList = $('#musicList'),
+			$listMask = $('.list-mask'),
+			$ctlList = $('.ctl-list');
+
+		var	play = new player.MusicPlayer($audio, $play, $curProgress, $musicList);
 		play.init();
 		
+		// 监听到音乐播放完毕后播放下一首音乐
 		$audio.addEventListener("ended", function(){
 			play.playNewMusic('next', $mode.data('value'));
 		});
 
+		// 设置音乐播放模式
 		$mode.click(function () {
 			play.changeMode($(this), $(this).data('value'));
 		});
 
+		// 播放上一首/下一首音乐
 		$next.click(function () {
 			play.playNewMusic($(this).data('value'), $mode.data('value'));
 		});
+
+		// 播放/暂停
 		$play.click(function () {
 			var value = $(this).data('value');
 			play.playMusic(value);
@@ -32,13 +39,14 @@ require([
 			$(this).attr('title', value === 'play' ? '暂停' : '播放');
 		});
 
-		$('.progress').click(function (e) {
+		// 调整音乐播放进度
+		$progress.click(function (e) {
 			var newPosition = e.pageX - parseInt( $('.music').css('marginLeft'));      // 当前鼠标点击位置 - 播放器左边的外间距
 			play.changeMusicProgress($(this), newPosition);
 		})
 
-		$('.ctl-list').click(function() {
-			var $musicList = $('.music-list');
+		// 查看音乐列表
+		$ctlList.click(function() {
 			var liCount = $musicList.find('li').length || 0;
 			var height = 0;
 			if(liCount * 40 > 320) {
@@ -47,89 +55,21 @@ require([
 				height = liCount * 40
 			}
 			$musicList.css('height', height + 'px');
-			$('.list-mask').css('display', 'block');
+			$listMask.css('display', 'block');
 		})
 
-		$('.list-mask').click(function() {
-			$('.music-list').css('height', '0px');
+		// 点击隐藏音乐列表
+		$listMask.click(function() {
+			$musicList.css('height', '0px');
 			$(this).css('display', 'none');
 		})
+
+		// 点击音乐列表中音乐时播放该音乐
+		$musicList.find('li').click(function() {
+			var index = $(this).data('index') || 0;
+			play.playMusicByIndex(index);
+		})
 	})();
-	// var audio = null;
-	// var timer = null;
-	// window.onload = function () {
-	// 	var play = new player();
-	// 	play.clList();
-		// audio = document.getElementById('music-audio');
-		// audio.addEventListener("canplay", function(){
-		// 	player.setMusicTime($('.all-time'), player.musicAllTime());
-		// });
-		// audio.addEventListener("ended", function(){
-		// 	player.lrcIndex = 0;
-		// 	console.log('ended');
-		// 	$('.music-data').css('transform', 'translate3d(0px, 0px, 0px)');
-		// 	if ($('#btnMode').data('value') === '单曲循环') {
-		// 		audio.loop = true;
-		// 		audio.load();
-		// 		player.playMusic('play');
-		// 		audio.loop = false;
-		// 	} else {
-		// 		player.playNewMusic('next', $('#btnMode').data('value'));
-		// 	}
-		// });
-
-		// $('.prevNext').click(function () {
-		// 	player.playNewMusic($(this).data('value'), $('#btnMode').data('value'));
-		// });
-		// $('#btnPlay').click(function () {
-		// 	var value = $(this).data('value');
-		// 	player.playMusic(value);
-		// 	$(this).data('value', value === 'play' ? 'pause' : 'play');
-		// 	$(this).attr('title', value === 'play' ? '暂停' : '播放');
-		// });
-		// $('#btnMode').click(function () {
-		// 	var value = $(this).data('value');
-		// 	$(this).removeClass('ctl-mode-random ctl-mode-single');
-		// 	var nextClass = '';
-		// 	var nextMode = '';
-		// 	switch (value) {
-		// 		case '列表循环':
-		// 			nextMode = '随机播放';
-		// 			nextClass = 'ctl-mode-random';
-		// 			break;
-		// 		case '随机播放':
-		// 			nextMode = '单曲循环';
-		// 			nextClass = 'ctl-mode-single';
-		// 			break;
-		// 		case '单曲循环':
-		// 			nextMode = '列表循环';
-		// 			nextClass = 'ctl-mode-list';
-		// 			break;
-		// 		default:
-		// 			break;
-		// 	}
-		// 	$(this).addClass(nextClass);
-		// 	$(this).data('value', nextMode);
-		// 	$(this).attr('title', nextMode);
-		// });
-
-		// $('.progress').click(function (e) {
-		// 	var newPosition = e.pageX - parseInt( $('.music').css('marginLeft'));      // 当前鼠标点击位置 - 播放器左边的外间距
-		// 	var beginProgress = $('.progress').position().left;   // 播放进度起始位置
-		// 	var allProgress = $('.progress').width();
-		// 	var allTime = player.musicAllTime();
-		// 	var currentTime;
-		// 	if (newPosition <= beginProgress) {
-		// 		currentTime = 0;
-		// 	} else if(newPosition >= (beginProgress + allProgress)) {
-		// 		currentTime = allTime;
-		// 	} else {
-		// 		currentTime = allTime * (newPosition - beginProgress) / allProgress;
-		// 	}
-		// 	console.log('setTime:' + currentTime);
-		// 	audio.currentTime = currentTime;
-		// })
-	// }
 });
 
 
--- js/player.js
+++ js/player.js
@@ -4,11 +4,11 @@ define([
     'use strict';
 
     function MusicPlayer($audio, $play, $curProgress, $musicList) {
-        // $('.current-progress')  
         this.timer = null;      
         this.lrcIndex = 0;
         this.playIndex = 0;
         this.musicList = list.musicList;
+        this.audio = $audio;
         this.initMusicList = function() {
             var $ul = $musicList.find('ul');
             $ul.html('');
@@ -22,73 +22,77 @@ define([
                     + '</li>');
             });
         }
+        // 页面打开时初始化第一首音乐
         this.init = function() {
             var newMusic = this.musicList[this.playIndex];
             $('.music-name span').text(newMusic.songname);
             $('.music-author span').text(newMusic.singername);
             $('.img-author').attr('src', newMusic.albumpic);
             $('.bacImg').css('backgroundImage','url(' + newMusic.albumpic + ')'); 
-            $audio.src = newMusic.url;
-            $audio.load();
+            this.audio.src = newMusic.url;
+            this.audio.load();
             this.initMusicList();
+            this.setMusicListActive(this.playIndex);
             // 为audio动态的更改音频路径，播放不同的音频时
             // 点击的时候，音频没有加载（虽然已经开始播放），获取不到时长，audio.duration为nan
             // 添加事件监听，当准备好音频时再获取时长
             var that = this;
-            $audio.addEventListener("canplay", function(){  
+            this.audio.addEventListener("canplay", function(){  
                 that.setMusicTime($('.all-time'), that.musicAllTime());
             });
         }
-
+        
+        // 播放/暂停音乐
         this.playMusic = function (playAction) {
-            // var audio = document.getElementById('music-audio');
             if (playAction === 'play') {
                 $play.addClass('ctl-pause');
                 $('.img-author').css('animation-play-state', 'running');
-                $audio.play();
+                this.audio.play();
                 this.setMusicProgress('play');		
             } else {
                 $play.removeClass('ctl-pause');
                 $('.img-author').css('animation-play-state', 'paused');
-                $audio.pause();
+                this.audio.pause();
                 this.setMusicProgress('pause');	
             }
             var that = this;
-            $audio.addEventListener("canplay", function(){
+            this.audio.addEventListener("canplay", function(){
                 that.setMusicTime($('.all-time'), that.musicAllTime());
             });
         },
 
+        // 设置音乐播放进度
         this.setMusicProgress = function (playStatus) {
             var that = this;
-            // var timer,
             var	allTime,
                 currentTime,
                 allProgressWidth;
 
             if (playStatus === 'play') {
+                // 设定定时器，根据播放时间调整播放进度
                 this.timer = setInterval(function () {
                     allTime = that.musicAllTime();
                     currentTime = that.musicCurrentTime();
-                    console.log('getTime:' + currentTime);
                     allProgressWidth = $('.progress').width();
                     that.setMusicTime($('.current-time'), currentTime);
                     $curProgress.width(currentTime / allTime * allProgressWidth);
-                    for(var i = that.lrcIndex; i < $('.music-data p').length; i++) {
-                        if ($('.music-data p').eq(i).data('time') <= parseInt(currentTime) && $('.music-data p').eq(i + 1).data('time') > parseInt(currentTime) ) {
-                            $('.music-data').css('transform', 'translate3d(0px, -' + 20 * i + 'px, 0px)');
-                            that.lrcIndex = i;
-                            break;
-                        }
-                    }
+                    // for(var i = that.lrcIndex; i < $('.music-data p').length; i++) {
+                    //     if ($('.music-data p').eq(i).data('time') <= parseInt(currentTime) && $('.music-data p').eq(i + 1).data('time') > parseInt(currentTime) ) {
+                    //         $('.music-data').css('transform', 'translate3d(0px, -' + 20 * i + 'px, 0px)');
+                    //         that.lrcIndex = i;
+                    //         break;
+                    //     }
+                    // }
                 }, 1000);
             }
 
             if (playStatus === 'pause') {
+                // 暂停时清除定时器
                 clearInterval(this.timer);
             }
         },
 
+        // 手动改变音乐播放进度
         this.changeMusicProgress = function ($musicProgress, newPosition) {
 			var beginProgress = $musicProgress.position().left;   // 播放进度起始位置
 			var allProgress = $musicProgress.width();
@@ -101,32 +105,31 @@ define([
 			} else {
 				currentTime = allTime * (newPosition - beginProgress) / allProgress;
 			}
-			$audio.currentTime = currentTime;
+			this.audio.currentTime = currentTime;
             this.setMusicTime($('.current-time'), currentTime);
             $curProgress.width(currentTime / allTime * allProgress);
         },
 
+        // 获取音乐总时间
         this.musicAllTime = function () {
-            // var audio = document.getElementById('music-audio');
-            return $audio.duration || 0;
+            return this.audio.duration || 0;
         },
 
+        // 获取音乐当前播放时间
         this.musicCurrentTime = function () {	
-            // var audio = document.getElementById('music-audio');
-            return ($audio.currentTime + 1) || 0;
+            return (this.audio.currentTime + 1) || 0;
         },
 
+        // 设置音乐跳转给定时间
         this.setMusicTime = function (element, time) {			
             var minutes = parseInt(time / 60);
             var seconds = parseInt(time % 60);
             element.text((minutes < 10 ? ('0' + minutes) : minutes) + ':' + (seconds < 10 ? ('0' + seconds) : seconds));
         },
 
+        // 播放新的音乐
         this.playNewMusic = function (prevNext, playMode) {
             this.setMusicTime($('.current-time'), 0);
-            // this.lrcIndex = 0;
-            // $('.music-data').css('transform', 'translate3d(0px, 0px, 0px)');
-            // var audio = document.getElementById('music-audio');
             switch (playMode) {
                 case '列表循环':                
                     if (prevNext === 'next') {
@@ -137,32 +140,16 @@ define([
                     }
                     break;
                 case '单曲循环':                
-                    $audio.loop = true;
-                    // audio.load();
-                    // player.playMusic('play');
-                    // audio.loop = false;
+                    this.audio.loop = true;
                     break;
                 case '随机播放':
                     this.playIndex = Math.round(Math.random() * (this.musicList.length - 1));
                     break;
             }
-            var newMusic = this.musicList[this.playIndex];
-            $('.music-name span').text(newMusic.songname);
-            $('.music-author span').text(newMusic.singername);
-            $('.img-author').attr('src', newMusic.albumpic);
-            $('.bacImg').css('backgroundImage','url(' + newMusic.albumpic + ')'); 
-            $audio.src = newMusic.url;
-            $audio.load();
-            this.playMusic('play');
-            // 为audio动态的更改音频路径，播放不同的音频时
-            // 点击的时候，音频没有加载（虽然已经开始播放），获取不到时长，audio.duration为nan
-            // 添加事件监听，当准备好音频时再获取时长
-            var that = this;
-            $audio.addEventListener("canplay", function(){  
-                that.setMusicTime($('.all-time'), that.musicAllTime());
-            });
+            this.playMusicByIndex(this.playIndex);
         },
 
+        // 设置音乐播放模式
         this.changeMode = function($mode, value) {
 			$mode.removeClass('ctl-mode-random ctl-mode-single');
 			var nextClass = '',
@@ -186,6 +173,34 @@ define([
 			$mode.addClass(nextClass);
 			$mode.data('value', nextMode);
 			$mode.attr('title', nextMode);
+        },
+
+        // 设置列表中当前播放音乐的样式
+        this.setMusicListActive = function(index) {
+            $('#musicList li').removeClass('play-on');
+            $('#musicList li').eq(index).addClass('play-on');
+        },
+
+        // 根据索引播放音乐（从音乐列表点击时调用该方法）
+        this.playMusicByIndex = function(index) {
+            this.playIndex = index;
+            this.setMusicTime($('.current-time'), 0);
+            var newMusic = this.musicList[this.playIndex];
+            $('.music-name span').text(newMusic.songname);
+            $('.music-author span').text(newMusic.singername);
+            $('.img-author').attr('src', newMusic.albumpic);
+            $('.bacImg').css('backgroundImage','url(' + newMusic.albumpic + ')'); 
+            this.setMusicListActive(this.playIndex);
+            this.audio.src = newMusic.url;
+            this.audio.load();
+            this.playMusic('play');
+            // 为audio动态的更改音频路径，播放不同的音频时
+            // 点击的时候，音频没有加载（虽然已经开始播放），获取不到时长，audio.duration为nan
+            // 添加事件监听，当准备好音频时再获取时长
+            var that = this;
+            this.audio.addEventListener("canplay", function(){  
+                that.setMusicTime($('.all-time'), that.musicAllTime());
+            });
         }
     }
 
